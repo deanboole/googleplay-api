d07f83439bb104763c57f89be29ac531d5e0bd68
deanboole <xspiritualx@gmail.com>
2016-01-12 16:16:35 +0800

    use virustotal package

--- vt.py
+++ vt.py
@@ -1,15 +1,24 @@
-import simplejson
-import urllib
-import urllib2
-
 import sys
+import virustotal
 
-url = "https://www.virustotal.com/vtapi/v2/url/report"
-parameters = {"resource": str(sys.argv[1]),
-              "apikey": "fill in your key"}
-data = urllib.urlencode(parameters)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-json = response.read()
+v = virustotal.VirusTotal("a264d77db499762fa7de5cf0372c2129a288ff38e02a81e8a4a736ec3667f214")
+# report = v.get("downloaded_apks/2016-01-06_TOOLS_2_com.cleanmaster.mguard.apk")
+report = v.scan(sys.argv[1])
 
-print json
+print "Report"
+print "- Resource's UID:", report.id
+print "- Scan's UID:", report.scan_id
+print "- Permalink:", report.permalink
+print "- Resource's SHA1:", report.sha1
+print "- Resource's SHA256:", report.sha256
+print "- Resource's MD5:", report.md5
+print "- Resource's status:", report.status
+print "- Antivirus' total:", report.total
+print "- Antivirus's positives:", report.positives
+for antivirus, malware in report:
+    if malware is not None:
+        print
+        print "Antivirus:", antivirus[0]
+        print "Antivirus' version:", antivirus[1]
+        print "Antivirus' update:", antivirus[2]
+        print "Malware:", malware
