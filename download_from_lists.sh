d4401396f21bb1b6b2634ac6224867dcc1a16aff
deanboole <xspiritualx@gmail.com>
2016-01-14 14:31:48 +0800

    orgnized folders

--- download_from_lists.sh
+++ download_from_lists.sh
@@ -7,18 +7,21 @@ categories=(APP_WIDGETS TOOLS WEATHER LIFESTYLE PRODUCTIVITY TRANSPORTATION SOCI
 for category in ${categories[@]}; do
 
     # grap lists
-    python list.py $category apps_topselling_free 10|cut -d';' -f 2 |grep -v "Package name" >> "$t_stamp"_"$category".txt
+    mkdir -p download/applist_todownload/$t_stamp
+    python list.py $category apps_topselling_free 1|cut -d';' -f 2 |grep -v "Package name" >> download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
 
     # grep details
-    python list.py $category apps_topselling_free 10 > "$t_stamp"_"$category"_detail.csv
+    mkdir -p download/apk_detail_info/$t_stamp
+    python list.py $category apps_topselling_free 1 > download/apk_detail_info/$t_stamp/"$t_stamp"_"$category"_detail.csv
 
     # download apks from lists
     n=1
     while read -r apk
     do
         python download.py "$apk"
-        mv "$apk".apk downloaded_apks/"$t_stamp"_"$category"_"$n"_"$apk".apk
+        mkdir -p download/downloaded_apks/$t_stamp
+        mv "$apk".apk download/downloaded_apks/$t_stamp/"$t_stamp"_"$category"_"$n"_"$apk".apk
 
         n=$(($n+1))
-    done < "$t_stamp"_"$category".txt
+    done < download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
 done
