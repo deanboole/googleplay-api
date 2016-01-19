c285e0919b568c227dd2be487928ff39ebafb29d
deanboole <xspiritualx@gmail.com>
2016-01-19 16:25:32 +0800

    change path

--- andro-analysis/androguard-report.sh
+++ andro-analysis/androguard-report.sh
@@ -6,6 +6,6 @@ if [ ! -d "andro-reports" ]; then
   mkdir -p andro-reports/$t_stamp
 fi
 
-for apk in $(ls download/downloaded_apks/$t_stamp); do
-    python androinfo.py download/downloaded_apks/$t_stamp/$apk > andro-reports/$t_stamp/ag_"$apk".txt
+for apk in $(ls ../download/downloaded_apks/$t_stamp); do
+    python androinfo.py ../download/downloaded_apks/$t_stamp/$apk > andro-reports/$t_stamp/ag_"$apk".txt
 done
--- virustotal/vt.sh
+++ virustotal/vt.sh
@@ -2,7 +2,7 @@
 
 t_stamp=`date +"%Y-%m-%d"`
 
-for apk in $(ls download/downloaded_apks/$t_stamp); do
-    python vt.py download/downloaded_apks/$t_stamp/$apk > vt-reports/vt_"$apk".txt
+for apk in $(ls ../download/downloaded_apks/$t_stamp); do
+    python vt.py ../download/downloaded_apks/$t_stamp/$apk > vt-reports/vt_"$apk".txt
     sleep 16
 done
