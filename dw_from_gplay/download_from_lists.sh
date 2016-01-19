792f9be72de0a203ee187e826c07d504841c6d46
deanboole <xspiritualx@gmail.com>
2016-01-19 16:34:08 +0800

    change path of downloaded info

--- dw_from_gplay/download_from_lists.sh
+++ dw_from_gplay/download_from_lists.sh
@@ -7,30 +7,30 @@ categories=(APP_WIDGETS TOOLS WEATHER LIFESTYLE PRODUCTIVITY TRANSPORTATION SOCI
 for category in ${categories[@]}; do
 
     # grap lists
-    if [ ! -d "download/applist_todownload/$t_stamp" ]; then
-        mkdir -p download/applist_todownload/$t_stamp
+    if [ ! -d "../download/applist_todownload/$t_stamp" ]; then
+        mkdir -p ../download/applist_todownload/$t_stamp
     fi
 
-    python list.py $category apps_topselling_free 1|cut -d';' -f 2 |grep -v "Package name" >> download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
+    python list.py $category apps_topselling_free 1|cut -d';' -f 2 |grep -v "Package name" >> ../download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
 
     # grep details
-    if [ ! -d "download/applist_todownload/$t_stamp" ]; then
-        mkdir -p download/apk_detail_info/$t_stamp
+    if [ ! -d "../download/applist_todownload/$t_stamp" ]; then
+        mkdir -p ../download/apk_detail_info/$t_stamp
     fi
 
-    python list.py $category apps_topselling_free 1 > download/apk_detail_info/$t_stamp/"$t_stamp"_"$category"_detail.csv
+    python list.py $category apps_topselling_free 1 > ../download/apk_detail_info/$t_stamp/"$t_stamp"_"$category"_detail.csv
 
     # download apks from lists
     n=1
     while read -r apk
     do
         python download.py "$apk"
-        if [ ! -d "download/downloaded_apks/$t_stamp" ]; then
-            mkdir -p download/downloaded_apks/$t_stamp
+        if [ ! -d "../download/downloaded_apks/$t_stamp" ]; then
+            mkdir -p ../download/downloaded_apks/$t_stamp
         fi
 
-        mv "$apk".apk download/downloaded_apks/$t_stamp/"$t_stamp"_"$category"_"$n"_"$apk".apk
+        mv "$apk".apk ../download/downloaded_apks/$t_stamp/"$t_stamp"_"$category"_"$n"_"$apk".apk
 
         n=$(($n+1))
-    done < download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
+    done < ../download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
 done
