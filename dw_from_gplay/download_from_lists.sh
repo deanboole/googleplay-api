6e12ea1d996055d759de5cbb38d4fcc94a52c2bd
sasaki <fatweikung@gmail.com>
2016-01-22 14:17:54 +0800

    Store APK info & file to MongoDB

--- dw_from_gplay/download_from_lists.sh
+++ dw_from_gplay/download_from_lists.sh
@@ -16,32 +16,34 @@ BOOKS_AND_REFERENCE MEDICAL ANDROID_WEAR \
 )
 
 for category in ${categories[@]}; do
+    python list.py $category apps_topselling_free 5
+    
     # grap lists
-    if [ ! -d "../download/applist_todownload/$t_stamp" ]; then
-        mkdir -p ../download/applist_todownload/$t_stamp
-    fi
+    #if [ ! -d "../download/applist_todownload/$t_stamp" ]; then
+    #    mkdir -p ../download/applist_todownload/$t_stamp
+    #fi
 
-    python list.py $category apps_topselling_free 1|cut -d';' -f 2 |grep -v "Package name" >> ../download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
+    #python list.py $category apps_topselling_free 1|cut -d';' -f 2 |grep -v "Package name" >> ../download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
 
     # grep details
-    if [ ! -d "../download/applist_detail_info/$t_stamp" ]; then
-        mkdir -p ../download/apk_detail_info/$t_stamp
-    fi
+    #if [ ! -d "../download/applist_detail_info/$t_stamp" ]; then
+    #    mkdir -p ../download/apk_detail_info/$t_stamp
+    #fi
 
-    python list.py $category apps_topselling_free 1 > ../download/apk_detail_info/$t_stamp/"$t_stamp"_"$category"_detail.csv
+    #python list.py $category apps_topselling_free 1 > ../download/apk_detail_info/$t_stamp/"$t_stamp"_"$category"_detail.csv
 
     # download apks from lists
-    n=1
-    while read -r apk
-    do
-        python download.py "$apk"
-        if [ ! -d "../download/downloaded_apks/$t_stamp" ]; then
-            mkdir -p ../download/downloaded_apks/$t_stamp
-        fi
+    #n=1
+    #while read -r apk
+    #do
+    #    python download.py "$apk"
+    #    if [ ! -d "../download/downloaded_apks/$t_stamp" ]; then
+    #        mkdir -p ../download/downloaded_apks/$t_stamp
+    #    fi
 
-        mv "$apk".apk ../download/downloaded_apks/$t_stamp/"$t_stamp"_"$category"_"$n"_"$apk".apk
+    #    mv "$apk".apk ../download/downloaded_apks/$t_stamp/"$t_stamp"_"$category"_"$n"_"$apk".apk
 
-        n=$(($n+1))
-    done < ../download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
+    #    n=$(($n+1))
+    #done < ../download/applist_todownload/$t_stamp/"$t_stamp"_"$category".txt
 done
 
